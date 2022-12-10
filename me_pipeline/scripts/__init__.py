94fb4358eccb53430a6cfbdfd966936689937738
Andrew Van <vanandrew@wustl.edu>
2022-12-10 16:20:46 -0600

    :bug: prefer internal scripts over external

--- me_pipeline/scripts/__init__.py
+++ me_pipeline/scripts/__init__.py
@@ -5,7 +5,7 @@ epilog = "Vahdeta Suljic <suljic@wustl.edu>, Andrew Van <vanandrew@wustl.edu>"
 
 # import bin scripts to path
 # subprocesses should inherit the PATH from the parent process
-os.environ["PATH"] += os.pathsep + os.path.join(os.path.dirname(__file__), "bin")
+os.environ["PATH"] = os.path.join(os.path.dirname(__file__), "bin") + os.pathsep + os.environ["PATH"]
 
 # setup data dir for scripts that need it
 os.environ["DATA_DIR"] = os.path.join(os.path.dirname(__file__), "data")
