86fe9e3cded1a2e56289bbedab5de71f31baa6b8
Andrew Van <vanandrew@wustl.edu>
2022-12-19 20:00:01 -0600

    :bug: fix path to data and scripts

--- me_pipeline/scripts/__init__.py
+++ me_pipeline/scripts/__init__.py
@@ -3,14 +3,14 @@ import os
 # set a default epilog signature
 epilog = "Vahdeta Suljic <suljic@wustl.edu>, Andrew Van <vanandrew@wustl.edu>"
 
-# import bin scripts to path
-# subprocesses should inherit the PATH from the parent process
-os.environ["PATH"] = os.path.join(os.path.dirname(__file__), "bin") + os.pathsep + os.environ["PATH"]
-
-# setup data dir for scripts that need it
-os.environ["DATA_DIR"] = os.path.join(os.path.dirname(__file__), "data")
-
 # Data and Bin directories
 # Use realpath to resolve symlinks in editable installs
 BIN_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), "bin")
 DATA_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), "data")
+
+# import bin scripts to path
+# subprocesses should inherit the PATH from the parent process
+os.environ["PATH"] = str(BIN_DIR) + os.pathsep + os.environ["PATH"]
+
+# setup data dir for scripts that need it
+os.environ["DATA_DIR"] = str(DATA_DIR)
