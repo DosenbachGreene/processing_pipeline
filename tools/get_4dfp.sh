a36a6d3f46056ad4bfab2d47d75770795820afac
Andrew Van <vanandrew@wustl.edu>
2023-04-27 00:46:26 -0500

    :sparkles: add backup links for 4dfp

--- tools/get_4dfp.sh
+++ tools/get_4dfp.sh
@@ -14,3 +14,12 @@ get refdir.tar
 bye
 EOF
 
+# UNCOMMENT BELOW IN-CASE OF EMERGENCIES
+# The links below are box links to a frozen working version of 4dfp tools.
+# Ideally the very latest 4dfp builds should be used with the pipeline
+# but in the inevitable case that something breaks you can use the lines
+# below instead of the ftp server.
+# wget https://wustl.box.com/shared/static/939yhwsqe9jxvx1wkham2f1bhl7s5e7w.tar -O 4dfp_scripts.tar
+# wget https://wustl.box.com/shared/static/gf4gshnf33h6d6vjijc2gmdt264kecrr.tar -O nil-tools.tar
+# wget https://wustl.box.com/shared/static/gamtc5a5cemoi85prtsq4iuawh0zcufm.tar -O refdir.tar
+
